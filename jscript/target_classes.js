7753925e8a9449537614c68d14ba854a2b4460da
vbookshelf <44088528+vbookshelf@users.noreply.github.com>
2018-11-19 14:38:17 +0200

    Added files

---
+++ jscript/target_classes.js
@@ -0,0 +1,11 @@
+
+
+TARGET_CLASSES = {
+  0: "healthy",
+  1: "few varrao, hive beetles",
+  2: "varroa, small hive beetles",
+  3: "ant problems",
+  4: "hive being robbed",
+  5: "missing queen"
+
+};
